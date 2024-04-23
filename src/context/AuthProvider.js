55cae1f4d1645313d42465838095e549cdda0f92
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-23 09:28:42 +0530

    Update AuthProvider.js

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
+    const [auth, setAuth] = useState(() => {
+        const user = localStorage.getItem('user');
+        return user ? JSON.parse(user) : {};
+    });
+
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(auth));
+    }, [auth]);
 
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
