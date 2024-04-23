dff73bb1bdbd9f57e8d368f0d17ea60e7ccee2cd
Dilshan Karunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2024-04-23 09:28:32 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import AuthContext from './context/AuthProvider';
 import Register from './Register';
 import Login from './Login';
+import Home from './components/pages/Home'; 
+
+function Default() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate('/home');
+  }, [navigate]);
+
+  return null;
+}
 
 function App() {
-
   return (
-    <main className="App">
-      <Login />
-    </main>
+    <Router>
+      <main className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Default />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
